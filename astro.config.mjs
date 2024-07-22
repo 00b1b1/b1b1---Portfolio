3e497cd7ceac13da2c7a9bd988edddebc40f1631
Ahmed Bibi <hola@b1b1.es>
2024-07-22 02:39:14 +0200

    Added sitemap, robots.txt and more

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://b1b1.es',
+  integrations: [sitemap(), robotsTxt()]
+});
